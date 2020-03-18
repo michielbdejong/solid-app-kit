493f14d87f0af1bca6b0486b34beb68da6b4ee01
Michiel de Jong <michiel@unhosted.org>
2020-03-18 15:26:46 +0100

    attempt to add cli script

---
+++ src/cli.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import { Server } from "..";
+import Debug from "debug";
+
+const debug = Debug("basic example");
+
+const appFolder = process.argv[2];
+console.log("Serving app from ", appFolder);
+
+// on startup:
+const server = new Server(
+  8080, // port
+  new URL("http://localhost:8080"), // audience for WebId-OIDC bearer tokens
+  appFolder, // statics path (your app goes here!)
+  "../.db" // NSS-compatible user database
+);
+debug("listening...");
+server.listen().catch(console.error.bind(console));
