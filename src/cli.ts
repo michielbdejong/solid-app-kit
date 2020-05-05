d87c5284935d607daefade5dca45111364da2554
Michiel de Jong <michiel@unhosted.org>
2020-05-05 09:19:26 +0200

    don't load certs if no TLS

--- src/cli.ts
+++ src/cli.ts
@@ -13,18 +13,24 @@ const config: ConstructorOptions = {
   httpsDomain: undefined,
   port: 8000,
   https: false,
-  cert: {
-    key: readFileSync("server.key"),
-    cert: readFileSync("server.cert")
-  },
+  cert: undefined,
   appFolder, // statics path (your app goes here!)
   dbFolder: "../.db", // NSS-compatible user database
   redisUrl: process.env.REDIS_URL
 };
+
 if (process.env.HTTPS_DOMAIN) {
   console.log(
     `Serving app from ${appFolder} on https://${process.env.HTTPS_DOMAIN}/`
   );
+  try {
+    config.cert = {
+      key: readFileSync("server.key"),
+      cert: readFileSync("server.cert")
+    };
+  } catch (e) {
+    throw new Error("Could not load ./server.key and ./server.cert");
+  }
   config.port = 443;
   config.https = true;
   config.httpsDomain = process.env.HTTPS_DOMAIN;
